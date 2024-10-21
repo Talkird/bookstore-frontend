8f99abb872cf8942d74edc7db3d82a032d6aeb7b
Martina2612 <martiabril2612@gmail.com>
2024-10-20 21:52:08 -0300

    popup añadir al carrito

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -29,10 +29,10 @@ function Navbar() {
         </div>
         <div className="flex items-center gap-6 text-xl">
           <NavbarLink to="/cart">
-            <ShoppingCart size={24} strokeWidth={1.5} />
+            <ShoppingCart size={40} strokeWidth={1.5} />
           </NavbarLink>
           <NavbarLink to="/account">
-            <User size={24} strokeWidth={1.5} />
+            <User size={40} strokeWidth={1.5} />
           </NavbarLink>
           <NavbarLink to="/login">
             <Button variant="outline" className="bg-white text-primary">
