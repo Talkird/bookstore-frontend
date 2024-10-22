32f50dd701a44d449a61ffb366324aace506f8cb
fedemuntaabski <fmuntaabski@uade.edu.ar>
2024-10-21 22:42:27 -0300

    nuevo frontend para account

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,22 +1,22 @@
-import { getEmail,getRole } from "../utils/token";
+import { getEmail, getRole } from "../utils/token";
 
 function Account() {
 
   return (
-    <div className="mx-auto mt-10 max-w-md rounded-lg bg-white p-6 shadow-md ">
-      <h2 className="mb-6 text-center text-3xl font-semibold text-gray-800">
-        Mi cuenta
+    <div className="mx-auto my-10 max-w-md rounded-lg bg-white p-8 shadow-lg border border-gray-200">
+      <h2 className="mb-8 text-center text-4xl font-bold">
+        Mi Cuenta
       </h2>
-      <div className="space-y-6">
-        <div className="flex items-center">
-          <label className="w-1/3 font-medium text-gray-700">Email:</label>
-          <div className="w-2/3 rounded border bg-gray-100 p-3 text-gray-800">
+      <div>
+        <div className="flex items-center justify-between mb-6">
+          <label className="w-1/3 font-medium text-lg text-gray-700">Email:</label>
+          <div className="w-2/3 rounded-lg border bg-gray-50 p-4 text-gray-900 shadow-inner">
             {getEmail()}
           </div>
         </div>
-        <div className="flex items-center">
-          <label className="w-1/3 font-medium text-gray-700">Rol:</label>
-          <div className="w-2/3 rounded border bg-gray-100 p-3 text-gray-800">
+        <div className="flex items-center justify-between mb-6">
+          <label className="w-1/3 font-medium text-lg text-gray-700">Rol:</label>
+          <div className="w-2/3 rounded-lg border bg-gray-50 p-4 text-gray-900 shadow-inner">
             {getRole()}
           </div>
         </div>
