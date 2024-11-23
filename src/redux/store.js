abb3626391dc867b64ac6f9cdbe9aa4c1b03104e
Martina2612 <martiabril2612@gmail.com>
2024-11-22 23:54:34 -0300

    orderSlice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import ratingReducer from "./slice/ratingSlice";
 import bookReducer from "./slice/bookSlice";
+import orderReducer from "./slice/orderSlice";
 
 export const store = configureStore({
   reducer: {
     ratings: ratingReducer,
     books: bookReducer,
+    orders: orderReducer,
   },
 });
